39da72cc54834327fc7457872c780370a9c8107d
NDzuki <281244945@qq.com>
2022-01-03 13:06:32 +0800

    update 5th_week

---
+++ geecache/cache.go
@@ -0,0 +1,27 @@
+package geecache
+
+import (
+	"sync"
+
+	"github.com/NDzuki/geeklearn/geecache/lru"
+)
+
+type cache struct {
+	mu         sync.Mutex
+	lru        *lru.Cache
+	cacheBytes int64
+}
+
+func (c *cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		return
+	}
+
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+
+	return
+}
---
+++ geecache/go.mod
@@ -0,0 +1,7 @@
+module geecache
+
+go 1.17
+
+replace github.com/NDzuki/geeklearn/geecache/lru => ./lru
+
+require github.com/NDzuki/geeklearn/geecache/lru v0.0.0-00010101000000-000000000000
